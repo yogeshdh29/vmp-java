71a50808445afc6ddf5e02b3581399850fac3ebf
Yogesh Dhonde <yogesh.dhonde@entrata.com>
2025-04-15 14:07:30 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,11 @@ ECR_REPO_URL=$1
 IMAGE_TAG=$2
 CONTAINER_NAME="vmp-java-app"
 
+echo "Setting AWS credentials..."
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+export AWS_DEFAULT_REGION=us-east-1
+
 echo "Logging in to ECR..."
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$ECR_REPO_URL"
 
