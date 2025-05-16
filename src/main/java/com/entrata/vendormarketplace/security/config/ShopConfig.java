3527ce9e3f9b4ec91a448f56dbd22bf236fc95b3
Yogesh Dhonde <yogesh.dhonde@entrata.com>
2025-05-16 20:58:13 +0530

    Implement Security before jwt
    role -> user roles ManyToMany

--- src/main/java/com/entrata/vendormarketplace/security/config/ShopConfig.java
+++ src/main/java/com/entrata/vendormarketplace/security/config/ShopConfig.java
@@ -1,4 +1,4 @@
-package com.entrata.vendormarketplace.config;
+package com.entrata.vendormarketplace.security.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
---
+++ src/main/java/com/entrata/vendormarketplace/security/user/ShopUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.entrata.vendormarketplace.security.user;
+
+import com.entrata.vendormarketplace.model.User;
+import com.entrata.vendormarketplace.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ShopUserDetailsService implements UserDetailsService  {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = Optional.ofNullable(userRepository.findByEmail(email))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!")) ;
+        return ShopUserDetails.buildUserDetails(user);
+    }
+}
